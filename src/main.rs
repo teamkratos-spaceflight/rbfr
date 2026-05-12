f18a8398853786aec630aae4278201a411615fa4
Ben <meowcat@meowcat.site>
2026-05-12 19:44:07 +0100

    more logging!

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,13 @@ fn main() -> ! {
     let mut imu: FakeImu = FakeImu::new();
 
     let mut state_tick: u32 = 0;
+    let tick: u32 = 0;
     let mut state = FlightState::Idle;
 
     let mut last_altitude: f32 = 0.0;
     let mut falling_counter: u8 = 0;
 
-    fn log<S: core::fmt::Write>(
+    fn log<S: ufmt::uWrite>(
         serial: &mut S,
         tick: u32,
         state: &FlightState,
@@ -37,10 +38,29 @@ fn main() -> ! {
         ay: f32,
         az: f32,
     ) {
-        let _ = writeln!(serial,
-                         "{},{:?},{:.2},{:.2},{:.2},{:.2}",
-                         tick, state, alt, ax, ay, az
-        );
+        let _ = ufmt::uwrite!(serial, "{},", tick);
+        match state {
+            FlightState::Idle => { let _ = ufmt::uwrite!(serial, "Idle"); },
+            FlightState::Armed => { let _ = ufmt::uwrite!(serial, "Armed"); },
+            FlightState::Ascent => { let _ = ufmt::uwrite!(serial, "Ascent"); },
+            FlightState::Descent => { let _ = ufmt::uwrite!(serial, "Descent"); },
+        }
+
+        fn print_float<S: ufmt::uWrite>(serial: &mut S, f: f32) {
+            let i = f as i32;
+            let d = ((f - i as f32) * 100.0).abs() as i32;
+            let _ = ufmt::uwrite!(serial, ",{}.", i);
+            if d < 10 {
+                let _ = ufmt::uwrite!(serial, "0");
+            }
+            let _ = ufmt::uwrite!(serial, "{}", d);
+        }
+
+        print_float(serial, alt);
+        print_float(serial, ax);
+        print_float(serial, ay);
+        print_float(serial, az);
+        let _ = ufmt::uwrite!(serial, "\r\n");
     }
 
 
@@ -52,6 +72,7 @@ fn main() -> ! {
         let altitude = FakeImu::altitude(&mut imu);
 
         let rising = altitude > last_altitude;
+        log(&mut serial, tick, &state, altitude, _ax, _ay, az);
 
         if rising {
             falling_counter = 0;
